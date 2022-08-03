04f24f8285315edf38f3ee40fb6eb308a2dcf9de
Hedge Fleming <hedgefleming@gmail.com>
2022-08-03 14:59:35 -0400

    Added file interpreter

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import sys
+
 from lex import Lexer
 from parse import Parser
 
-if __name__ == "__main__":
-    lexer = Lexer()
-    parser = Parser()
 
+def shell(lexer: Lexer, parser: Parser):
+    """Start the interactive shell."""
     while True:
         source = input(">>> ").strip().split("#")[0]
         if source == "exit":
@@ -16,3 +17,13 @@ if __name__ == "__main__":
         result = ast.visit()
 
         print(result.repr())
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        print("Starting Echo* interpreter.")
+        with open(sys.argv[1]) as source:
+            Parser().parse(Lexer().tokenize(source.read())).visit()
+    else:
+        print("Starting interactive Echo* interpreter.")
+        shell(Lexer(), Parser())

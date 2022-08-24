db63ddbd83e263667558d08639e06caf0ef6725a
Hedge Fleming <hedgefleming@gmail.com>
2022-08-24 12:53:58 -0400

    tried to add more for-loop/if-expr variants

--- parse.py
+++ parse.py
@@ -54,112 +54,125 @@ class Parser(sly.Parser):
         """Function call, without arguments"""
         return CallNode(p.or_expr, [])
 
-    @_("LPAREN expr IF or_expr ELSE expr RPAREN")
+    @_("LPAREN expr IF or_expr ELSE expr RPAREN", "LPAREN program IF or_expr ELSE program RPAREN")
     def expr(self, p):
         """If-expression"""
-        return IfNode(p.expr0, p.or_expr, p.expr1)
+        return IfNode(p[1], p.or_expr, p[-2])
 
-    @_("LPAREN or_expr FOR IDENT IN or_expr RPAREN")
+    @_("LPAREN expr FOR IDENT IN or_expr RPAREN", "LPAREN program FOR IDENT IN or_expr RPAREN")
     def expr(self, p):
         """For-loop expression"""
-        return ForNode(p.or_expr1, p.IDENT, p.or_expr0)
+        return ForNode(p[1], p.IDENT, p[-2])
+
+    @_(
+        "IF or_expr LPAREN expr RPAREN ELSE LPAREN expr RPAREN",
+        "IF or_expr LPAREN program RPAREN ELSE LPAREN program RPAREN",
+    )
+    def expr(self, p):
+        """If-expression"""
+        return IfNode(p[3], p.or_expr, p[-2])
+
+    @_("FOR IDENT IN or_expr LPAREN expr RPAREN", "FOR IDENT IN or_expr RPAREN program RPAREN")
+    def expr(self, p):
+        """For-loop expression"""
+        return ForNode(p.or_expr, p.IDENT, p[-2])
 
     @_("or_expr")
     def expr(self, p):
         """Or-expression"""
         return p.or_expr
 
-    @_("and_expr OR and_expr")
+    @_("or_expr OR and_expr")
     def or_expr(self, p):
         """Or-expression"""
-        return BinOp("||", p.and_expr0, p.and_expr1)
+        return BinOp("||", p.or_expr, p.and_expr)
 
     @_("and_expr")
     def or_expr(self, p):
         """And-expression"""
         return p.and_expr
 
-    @_("equals_expr AND equals_expr")
+    @_("and_expr AND equals_expr")
     def and_expr(self, p):
         """And-expression"""
-        return BinOp("&&", p.equals_expr0, p.equals_expr1)
+        return BinOp("&&", p.and_expr, p.equals_expr)
 
     @_("equals_expr")
     def and_expr(self, p):
         """Equals or not-equals expression"""
         return p.equals_expr
 
-    @_("comp_expr EQEQ comp_expr")
+    @_("equals_expr EQEQ comp_expr")
     def equals_expr(self, p):
         """Equals-expression"""
-        return BinOp("==", p.comp_expr0, p.comp_expr1)
+        return BinOp("==", p.equals_expr, p.comp_expr)
 
-    @_("comp_expr NTEQ comp_expr")
+    @_("equals_expr NTEQ comp_expr")
     def equals_expr(self, p):
         """Not-equals expression"""
-        return BinOp("!=", p.comp_expr0, p.comp_expr1)
+        return BinOp("!=", p.equals_expr, p.comp_expr)
 
     @_("comp_expr")
     def equals_expr(self, p):
         """Comparison expression"""
         return p.comp_expr
 
-    @_("plus_expr LT plus_expr")
+    @_("comp_expr LT plus_expr")
     def comp_expr(self, p):
         """Less-than expression"""
-        return BinOp("<", p.plus_expr0, p.plus_expr1)
+        return BinOp("<", p.comp_expr, p.plus_expr)
 
-    @_("plus_expr GT plus_expr")
+    @_("comp_expr GT plus_expr")
     def comp_expr(self, p):
         """Greater-than expression"""
-        return BinOp(">", p.plus_expr0, p.plus_expr1)
+        return BinOp(">", p.comp_expr, p.plus_expr)
 
-    @_("plus_expr LTEQ plus_expr")
+    @_("comp_expr LTEQ plus_expr")
     def comp_expr(self, p):
         """Less-than expression"""
-        return BinOp("<=", p.plus_expr0, p.plus_expr1)
+        return BinOp("<=", p.comp_expr, p.plus_expr)
 
-    @_("plus_expr GTEQ plus_expr")
+    @_("comp_expr GTEQ plus_expr")
     def comp_expr(self, p):
         """Greater-than expression"""
-        return BinOp(">=", p.plus_expr0, p.plus_expr1)
+        return BinOp(">=", p.comp_expr, p.plus_expr)
 
     @_("plus_expr")
     def comp_expr(self, p):
         """Addition or subtraction expression"""
         return p.plus_expr
 
-    @_("mul_expr PLUS mul_expr")
+    @_("plus_expr PLUS mul_expr")
     def plus_expr(self, p):
         """Addition expression"""
-        return BinOp("+", p.mul_expr0, p.mul_expr1)
+        return BinOp("+", p.plus_expr, p.mul_expr)
 
-    @_("mul_expr MINUS mul_expr")
+    @_("plus_expr MINUS mul_expr")
     def plus_expr(self, p):
         """Subtraction expression"""
-        return BinOp("-", p.mul_expr0, p.mul_expr1)
+        return BinOp("-", p.plus_expr, p.mul_expr)
 
     @_("mul_expr")
     def plus_expr(self, p):
-        """Multiplication, division, or modulus expression"""
+        """Multiplication, division, modulus, or range expression"""
         return p.mul_expr
 
-    @_("atom MUL atom")
+    @_("mul_expr MUL atom")
     def mul_expr(self, p):
         """Multiplication expression"""
-        return BinOp("*", p.atom0, p.atom1)
+        return BinOp("*", p.mul_expr, p.atom)
 
-    @_("atom DIV atom")
+    @_("mul_expr DIV atom")
     def mul_expr(self, p):
         """Division expression"""
-        return BinOp("/", p.atom0, p.atom1)
+        return BinOp("/", p.mul_expr, p.atom)
 
-    @_("atom MOD atom")
+    @_("mul_expr MOD atom")
     def mul_expr(self, p):
         """Modulus expression"""
-        return BinOp("%", p.atom0, p.atom1)
+        return BinOp("%", p.mul_expr, p.atom)
 
-    @_("atom TO atom")
+    @_("mul_expr TO atom")
     def mul_expr(self, p):
         """Range definition"""
         return RangeNode(p.atom0, p.atom1)
@@ -236,6 +249,34 @@ class Parser(sly.Parser):
     def atom(self, p):
         """Function definition"""
         return Function(p.expr, [], name=p.IDENT)
+
+    @_("FN LPAREN func_params RPAREN ARROW LPAREN program RPAREN")
+    def atom(self, p):
+        """Function definition"""
+        return (
+            Function(p.program, list(p.func_params))
+            if isinstance(p.func_params, tuple)
+            else Function(p.program, [p.func_params])
+        )
+
+    @_("FN LPAREN RPAREN ARROW LPAREN program RPAREN")
+    def atom(self, p):
+        """Function definition"""
+        return Function(p.expr, [])
+
+    @_("FN IDENT LPAREN func_params RPAREN ARROW LPAREN program RPAREN")
+    def atom(self, p):
+        """Function definition"""
+        return (
+            Function(p.program, list(p.func_params), name=p.IDENT)
+            if isinstance(p.func_params, tuple)
+            else Function(p.program, [p.func_params], name=p.IDENT)
+        )
+
+    @_("FN IDENT LPAREN RPAREN ARROW LPAREN program RPAREN")
+    def atom(self, p):
+        """Function definition"""
+        return Function(p.program, [], name=p.IDENT)
 
     @_("IDENT COMMA func_params")
     def func_params(self, p):

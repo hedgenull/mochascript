bb9314c2adbc66e9157602c337017114c7150d86
Hedge Fleming <hedgefleming@gmail.com>
2022-08-29 12:36:51 -0400

    Added docstrings to unit tests

--- tests/test_number.py
+++ tests/test_number.py
@@ -4,8 +4,10 @@ from obj_model import *
 
 
 class NumberTestCase(unittest.TestCase):
-    """Test cases for the Number object"""
+    """Test cases for the Number object."""
+
     def test_addition(self):
+        """Test addition of two Numbers."""
         one = Number(1)
         three = Number(3)
         four = Number(4)
@@ -13,6 +15,7 @@ class NumberTestCase(unittest.TestCase):
         self.assertEqual(one.add(three), four, "Addition failed")
 
     def test_subtraction(self):
+        """Test subtraction of two Numbers."""
         five = Number(5)
         two = Number(2)
         three = Number(3)
@@ -20,6 +23,7 @@ class NumberTestCase(unittest.TestCase):
         self.assertEqual(five.sub(two), three, "Subtraction failed")
 
     def test_multiplication(self):
+        """Test multiplication of two Numbers."""
         four = Number(4)
         three = Number(3)
         twelve = Number(12)
@@ -27,13 +31,15 @@ class NumberTestCase(unittest.TestCase):
         self.assertEqual(four.mul(three), twelve, "Multiplication failed")
 
     def test_division(self):
+        """Test division of two Numbers."""
         five = Number(5)
         two = Number(2)
         two_point_five = Number(2.5)
 
         self.assertAlmostEqual(five.div(two), two_point_five, "Division failed")
 
-    def test_modulo(self):
+    def test_modulus(self):
+        """Test modulus of two Numbers."""
         five = Number(5)
         two = Number(2)
         one = Number(1)
@@ -41,6 +47,7 @@ class NumberTestCase(unittest.TestCase):
         self.assertAlmostEqual(five.mod(two), one, "Modulo failed")
 
     def test_positive(self):
+        """Test positive of a negative Number."""
         negative_two = Number(-2)
         two = Number(2)
 
@@ -49,6 +56,7 @@ class NumberTestCase(unittest.TestCase):
         )
 
     def test_negative(self):
+        """Test negative of a positive Number."""
         negative_two = Number(-2)
         two = Number(2)
 
--- tests/test_string.py
+++ tests/test_string.py
@@ -5,7 +5,9 @@ from obj_model import *
 
 class StringTestCase(unittest.TestCase):
     """Test cases for the String object."""
+
     def test_concatenation_with_string(self):
+        """Test concatenation of two Strings."""
         string1 = String("A")
         string2 = String("B")
         expected = String("AB")
@@ -13,6 +15,7 @@ class StringTestCase(unittest.TestCase):
         self.assertEqual(string1.add(string2), expected, "String-string concatenation failed")
 
     def test_concatenation_with_other(self):
+        """Test concatenation of a String and a non-String object."""
         string = String("123")
         array = Array([Number(1), Number(2), Number(3)])
         expected = String("123[1, 2, 3]")
@@ -20,6 +23,7 @@ class StringTestCase(unittest.TestCase):
         self.assertEqual(string.add(array), expected, "String-other concatenation failed")
 
     def test_subtraction(self):
+        """Test subtraction of two Strings."""
         string1 = String("abcdef")
         string2 = String("abc")
         expected = String("def")
@@ -27,20 +31,23 @@ class StringTestCase(unittest.TestCase):
         self.assertEqual(string1.sub(string2), expected, "String subtraction failed")
 
     def test_multiplication(self):
+        """Test multiplication of a String and a Number."""
         string = String("ko")
         three = Number(3)
         expected = String("kokoko")
 
         self.assertEqual(string.mul(three), expected, "String multiplication failed")
 
-    def test_division(self):
+    def test_indexing(self):
+        """Test String indexing (division with a Number)."""
         string = String("abcdef")
         zero = Number(0)
         expected = String("a")
 
-        self.assertEqual(string.div(zero), expected, "String division failed")
+        self.assertEqual(string.div(zero), expected, "String indexing/division failed")
 
     def test_format(self):
+        """Test String formatting (modulo with another String)."""
         hello = String("hello {}!")
         world = String("world")
         expected = String("hello world!")
